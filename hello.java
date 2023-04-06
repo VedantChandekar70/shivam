439b810b0821cda9b4c1086e2a6d933456f65a45
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-04-06 14:27:22 +0530

    Partners

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ public class hello {
     public static void main(String [] args) {
         System.out.println("vedant and Shivam");
         System.out.println("I am Priyanshu Gour");
+        System.out.println("Hey me and my partner is vedant");
     }
 
 }
